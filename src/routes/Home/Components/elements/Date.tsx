a5d80465a77ac0b796c2fdca98e864446942f196
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2026-05-20 22:29:08 +0300

    ss

--- src/routes/Home/Components/elements/Date.tsx
+++ src/routes/Home/Components/elements/Date.tsx
@@ -2,9 +2,17 @@
 
 import React from 'react';
 import { humanDuration } from './../../../../utils';
+import { ClientOnly } from './../../../../components/layout/ClientOnly';
+import { Skeleton } from './../../../../components/ui/skeleton';
 
-const Component = ({ from, to, className = 'mb-5 text-[10px] text-white text-opacity-70 uppercase' }) => (
-    <p className={className}>({humanDuration({ from, to })})</p>
+const Component = ({ from, to, className = 'mb-5 text-[10px] text-white text-opacity-70 uppercase' }: {
+    from: any;
+    to: any;
+    className: string;
+}) => (
+    <p className={className}>(<ClientOnly fallback={<Skeleton className="w-16 h-4 rounded-md"/>}>
+    {humanDuration({ from, to })}    
+    </ClientOnly>)</p>
 );
 
 export default Component;
