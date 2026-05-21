7846fc3b27accdd1231a8bba0699b84f70c2224e
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2026-05-21 13:29:16 +0300

    ss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-// module.exports = {
-//   plugins: {
-//     '@tailwindcss/postcss': {},
-//     autoprefixer: {},
-//   },
-// } 
-
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+	plugins: {
+		'@tailwindcss/postcss': {},
+		autoprefixer: {},
+	},
+}  
