efa9f1ba1dddeaf96d09bef2469448aa2dad6df2
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2026-08-02 01:22:20 +0300

    ss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import routes, { queryClient } from './routes';
 import { AuthProvider } from './auth';
